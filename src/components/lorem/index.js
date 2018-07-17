b9ec2547035cd3caadc08f6dbd59ac442eb98b7f
Salvatore Laisa <moebiusmania@gmail.com>
2018-07-17 11:02:04 +0200

    updated dependencies, better link contrast, add PWA manifest (no icons)

--- src/components/lorem/index.js
+++ src/components/lorem/index.js
@@ -7,7 +7,7 @@ export default class Lorem extends React.Component {
     return (
       <div>
         <p>Welcome to your next project, let's get started! 🎉</p>
-        <p>For the few options please refer to the <a target="_blank" href={url}>readme file</a> on the project repo</p>
+        <p>For the few options please refer to the <a target="_blank" href={url} rel="noopener">readme file</a> on the project repo</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sapien turpis, posuere vitae metus sed, pulvinar mollis nisl. Cras eu enim nec lacus faucibus laoreet. Phasellus sed commodo ipsum, vitae rutrum sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p>
         <p><small>(this is just a dummy element, please remove it 😎)</small></p>
       </div>
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackNotifierPlugin = require('webpack-notifier');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const pkg = require('./package.json');
 
 const env = process.env.NODE_ENV || 'development';
@@ -24,7 +25,21 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       title: pkg.name,
+      description: pkg.description,
       template: './src/assets/index.html'
+    }),
+    new WebpackPwaManifest({
+      name: pkg.name,
+      short_name: pkg.name,
+      description: pkg.description,
+      background_color: '#ffffff',
+      theme_color: '#147EB2'
+      // icons: [
+      //   {
+      //     src: path.resolve('src/assets/icon.png'),
+      //     sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+      //   }
+      // ]
     })
   ],
   devtool: 'source-map',
